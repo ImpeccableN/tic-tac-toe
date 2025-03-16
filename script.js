75a90b15d249c59a4925358ef5eda5341e1836cf
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2025-03-16 16:33:04 +0100

    fixed the cheForWin function in Gameboard IIFE

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const Gameboard = (function () {
     const resetBoard = () => {board = emptyBoard};
 
     const checkForWin = function() {
-        if(board[0] !=" "){
+        if(board[0] != " "){
             if(board[0] == board[1] && board[0] == board[2]){
                 //need to change
                 alert("You won!");
@@ -32,12 +32,14 @@ const Gameboard = (function () {
                 //need to change
                 alert("You won!");
             }
-        } else if(board[3] != " ") {
+        } 
+        if(board[3] != " ") {
             if(board[3] == board[4] && board[3] == board[5]){
                 //need to change
                 alert("You won!");
             }
-        } else if(board[6] != " ") {
+        } 
+        if(board[6] != " ") {
             if(board[6] == board[7] && board[6] == board[8]){
                 //need to change
                 alert("You won!");
@@ -45,16 +47,21 @@ const Gameboard = (function () {
                 //need to change
                 alert("You won!");
             }
-        } else if(board[1] != " ") {
+        } 
+        if(board[1] != " ") {
             if(board[1] == board[4] && board[1] == board[7]){
                 //need to change
                 alert("You won!");
             }
-        } else if(board[2] != " ") {
+        } 
+        if(board[2] != " ") {
             if(board[2] == board[5] && board[2] == board[8]){
                 //need to change
                 alert("You won!");
             }
+        } 
+        if(!board.includes(" ")) {
+            alert("It's a tie!");
         }
     };
 
@@ -70,7 +77,15 @@ const Gameloop = (function() {
     const player2 = createPlayer("Niels", "O");
 
     Gameboard.drawBoard();
-    let input = prompt(player1.name + ", your turn.");
-    Gameboard.fillField(parseInt(input), player1.symbol);
-    Gameboard.drawBoard();
+    for (i = 0; i < 5; i++){
+        let input = prompt(player1.name + ", your turn.");
+        Gameboard.fillField(parseInt(input), player1.symbol);
+        Gameboard.drawBoard();
+        Gameboard.checkForWin();
+        let input2 = prompt(player2.name + ", your turn.");
+        Gameboard.fillField(parseInt(input2), player2.symbol);
+        Gameboard.drawBoard();
+        Gameboard.checkForWin();
+    }
+    return {player1, player2}
 })();
