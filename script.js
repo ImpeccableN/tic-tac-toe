b1033bfc5a9d720b84dfec2e025748eeb01c7b46
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2025-03-16 17:09:24 +0100

    shortened the Gameloop IIFE using currentplayer variable

--- script.js
+++ script.js
@@ -24,48 +24,36 @@ const Gameboard = (function () {
     const checkForWin = function() {
         if(board[0] != " "){
             if(board[0] == board[1] && board[0] == board[2]){
-                //need to change
-                alert("You won!");
+                return true;
             } else if(board[0] == board[3] && board[0] == board[6]){
-                //need to change
-                alert("You won!");
+                return true;
             } else if(board[0] == board[4] && board[0] == board[8]){
-                //need to change
-                alert("You won!");
+                return true;
             }
         } 
         if(board[3] != " ") {
             if(board[3] == board[4] && board[3] == board[5]){
-                //need to change
-                alert("You won!");
+                return true;
             }
         } 
         if(board[6] != " ") {
             if(board[6] == board[7] && board[6] == board[8]){
-                //need to change
-                alert("You won!");
+                return true;
             } else if(board[6] == board[4] && board[6] == board[2]){
-                //need to change
-                alert("You won!");
+                return true;
             }
         } 
         if(board[1] != " ") {
             if(board[1] == board[4] && board[1] == board[7]){
-                //need to change
-                alert("You won!");
+                return true;
             }
         } 
         if(board[2] != " ") {
             if(board[2] == board[5] && board[2] == board[8]){
-                //need to change
-                alert("You won!");
+                return true;
             }
-        } 
-        if(!board.includes(" ")) {
-            alert("It's a tie!");
         }
     };
-
     return {fillField, drawBoard, resetBoard, checkForWin}
 })();
 
@@ -76,20 +64,21 @@ function createPlayer (name, symbol) {
 const Gameloop = (function() {
     const player1 = createPlayer("Antje", "X");
     const player2 = createPlayer("Niels", "O");
+    let currentPlayer = player1;
 
     Gameboard.drawBoard();
-    for (i = 0; i < 5; i++){
-        let input = prompt(player1.name + ", your turn.");
-        Gameboard.fillField(parseInt(input), player1.symbol)
+    for (i = 0; i < 9; i++){
+        let input = prompt(currentPlayer.name + ", your turn.");
+        Gameboard.fillField(parseInt(input), currentPlayer.symbol)
         Gameboard.drawBoard();
-        Gameboard.checkForWin();
-        let input2 = prompt(player2.name + ", your turn.");
-        Gameboard.fillField(parseInt(input2), player2.symbol);
-        Gameboard.drawBoard();
-        Gameboard.checkForWin();
+        if (Gameboard.checkForWin()){
+            alert(currentPlayer.name + " wins!");
+            return;
+        };
+        if (currentPlayer == player1){
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        };
     }
-
-
-
-    return {player1, player2}
 })();
